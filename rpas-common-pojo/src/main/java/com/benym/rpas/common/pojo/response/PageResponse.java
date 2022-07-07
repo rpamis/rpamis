d0e477a6d25fc0ebab2f1d0e0ae1dcdaf69a887a
benym <38175888+benym@users.noreply.github.com>
2022-07-07 20:29:45 +0800

    [feature]分页统一返回对象

---
+++ rpas-common-pojo/src/main/java/com/benym/rpas/common/pojo/response/PageResponse.java
@@ -0,0 +1,75 @@
+package com.benym.rpas.common.pojo.response;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @date 2022/7/7 7:53 下午
+ */
+public class PageResponse<T> {
+
+    private static final long serialVersionUID = 1L;
+
+    private int totalCount = 0;
+
+    private int pageIndex = 1;
+
+    private int pageSize = 1;
+
+    private Collection<T> data;
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public int getPageIndex() {
+        if (pageIndex < 1) {
+            return 1;
+        }
+        return pageIndex;
+    }
+
+    public void setPageIndex(int pageIndex) {
+        if (pageIndex < 1) {
+            this.pageIndex = 1;
+        } else {
+            this.pageIndex = pageIndex;
+        }
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public List<T> getData() {
+        if (null == data) {
+            return Collections.emptyList();
+        }
+        if (data instanceof List) {
+            return (List<T>) data;
+        }
+        return new ArrayList<>(data);
+    }
+
+    public void setData(Collection<T> data) {
+        this.data = data;
+    }
+
+    public boolean isEmpty() {
+        return data == null || data.isEmpty();
+    }
+
+    public boolean isNotEmpty() {
+        return !isEmpty();
+    }
+}
