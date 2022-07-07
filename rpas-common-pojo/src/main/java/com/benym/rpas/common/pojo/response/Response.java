fd443b05efb5fd81a81d2550468fcca05d82a154
benym <38175888+benym@users.noreply.github.com>
2022-07-07 19:39:35 +0800

    [feature]更新统一返回

--- rpas-common-pojo/src/main/java/com/benym/rpas/common/pojo/response/Response.java
+++ rpas-common-pojo/src/main/java/com/benym/rpas/common/pojo/response/Response.java
@@ -1,12 +1,21 @@
 package com.benym.rpas.common.pojo.response;
 
+import com.benym.rpas.common.pojo.enums.ResponseCode;
+import com.benym.rpas.common.pojo.enums.StatusCode;
+import java.io.Serializable;
+
 /**
  * @Time : 2022/7/6 21:17
  */
-public class Response<T> {
-    private int code;
+public class Response<T> implements Serializable {
 
-    private String message;
+    private static final long serialVersionUID = 1L;
+
+    private int errCode;
+
+    private String errMessage;
+
+    private String detailMessage;
 
     private T data;
 
@@ -14,26 +23,101 @@ public class Response<T> {
 
     }
 
-    public Response(int code, String message, T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
+    public Response(int errCode, String errMessage, String detailMessage) {
+        this.errCode = errCode;
+        this.errMessage = errMessage;
+        this.detailMessage = detailMessage;
     }
 
-    public int getCode() {
-        return code;
+    public Response(int errCode, String errMessage) {
+        this(errCode, errMessage, "");
     }
 
-    public void setCode(int code) {
-        this.code = code;
+    public Response(T data) {
+        errCode = 0;
+        errMessage = "";
+        setData(data);
     }
 
-    public String getMessage() {
-        return message;
+    public Response(StatusCode statusCode) {
+        errCode = statusCode.getCode();
+        errMessage = statusCode.getMessage();
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public static <T> Response<T> success() {
+        return new Response<>(ResponseCode.SUCCESS);
+    }
+
+    public static <T> Response<T> success(T data, int errCode, String errMessage) {
+        Response<T> response = new Response<>();
+        response.setData(data);
+        response.setErrCode(errCode);
+        response.setErrMessage(errMessage);
+        return response;
+    }
+
+    public static <T> Response<T> success(T data) {
+        return success(data, ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getMessage());
+    }
+
+    public static <T> Response<T> success(T data, String errMessage) {
+        return success(data, ResponseCode.SUCCESS.getCode(), errMessage);
+    }
+
+
+    public static <T> Response<T> fail(int errCode, String errMessage) {
+        Response<T> response = new Response<>();
+        response.setErrCode(errCode);
+        response.setErrMessage(errMessage);
+        return response;
+    }
+
+    public static <T> Response<T> fail() {
+        return fail(ResponseCode.FAILED.getCode(), ResponseCode.FAILED.getMessage());
+    }
+
+    public static <T> Response<T> fail(StatusCode statusCode) {
+        return fail(statusCode.getCode(), statusCode.getMessage());
+    }
+
+    public static <T> Response<T> fail(StatusCode statusCode, String detailMessage) {
+        Response<T> response = new Response<>();
+        response.setErrCode(statusCode.getCode());
+        response.setErrMessage(statusCode.getMessage());
+        response.setDetailMessage(detailMessage);
+        return response;
+    }
+
+    public static <T> Response<T> fail(T data, int errCode, String errMessage) {
+        Response<T> response = new Response<>();
+        response.setData(data);
+        response.setErrCode(errCode);
+        response.setErrMessage(errMessage);
+        return response;
+    }
+
+    public int getErrCode() {
+        return errCode;
+    }
+
+    public void setErrCode(int errCode) {
+        this.errCode = errCode;
+    }
+
+    public String getErrMessage() {
+        return errMessage;
+    }
+
+    public void setErrMessage(String errMessage) {
+        this.errMessage = errMessage;
+    }
+
+    public String getDetailMessage() {
+        return detailMessage;
+    }
+
+    public void setDetailMessage(String detailMessage) {
+        this.detailMessage = detailMessage;
     }
 
     public T getData() {
@@ -42,5 +126,15 @@ public class Response<T> {
 
     public void setData(T data) {
         this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "errCode=" + errCode +
+                ", errMessage='" + errMessage + '\'' +
+                ", detailMessage='" + detailMessage + '\'' +
+                ", data=" + data +
+                '}';
     }
 }
