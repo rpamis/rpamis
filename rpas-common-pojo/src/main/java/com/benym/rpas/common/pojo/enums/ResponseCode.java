78fad6cd683220feb8f834f1b9fa378feaf72a4d
benym <38175888+benym@users.noreply.github.com>
2022-07-07 19:40:16 +0800

    [feature]更新返回code

--- rpas-common-pojo/src/main/java/com/benym/rpas/common/pojo/enums/ResponseCode.java
+++ rpas-common-pojo/src/main/java/com/benym/rpas/common/pojo/enums/ResponseCode.java
@@ -4,14 +4,14 @@ package com.benym.rpas.common.pojo.enums;
  * @Time : 2022/7/6 21:18
  */
 
-public enum ResponseCode implements StatusCode{
+public enum ResponseCode implements StatusCode {
 
-    SUCCESS(20000,"请求成功"),
-    FAILED(20001,"请求失败"),
-    VALIDATE_ERROR(20002,"参数校验失败");
+    SUCCESS(0, "请求成功"),
+    FAILED(500, "请求失败"),
+    VALIDATE_ERROR(20000, "参数校验失败");
 
-    private int code;
-    private String message;
+    private final int code;
+    private final String message;
 
     ResponseCode(int code, String message) {
         this.code = code;
