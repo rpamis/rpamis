08241dc5b8f9bcd42d5e1c2899591fdde1ef0e58
benym <38175888+benym@users.noreply.github.com>
2022-07-11 16:01:49 +0800

    [feature]完善全局异常

--- rpas-common-core/src/main/java/com/benym/rpas/common/core/exception/ExceptionErrorHandler.java
+++ rpas-common-core/src/main/java/com/benym/rpas/common/core/exception/ExceptionErrorHandler.java
@@ -12,9 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Objects;
@@ -28,7 +29,6 @@ public class ExceptionErrorHandler {
     private static final Logger logger = LoggerFactory.getLogger(ExceptionErrorHandler.class);
 
     @ExceptionHandler(org.springframework.validation.BindException.class)
-    @ResponseStatus(value = HttpStatus.OK)
     public ResponseEntity<Response<Object>> handleBindException(BindException bindException) {
         final Trace trace = TraceIdUtils.getTrace();
         String validateMessage = Objects.requireNonNull(bindException.getFieldError()).getDefaultMessage();
@@ -37,6 +37,32 @@ public class ExceptionErrorHandler {
             logger.error(bindException.getMessage(), bindException);
         }
         final Response<Object> failResponse = Response.fail(ResponseCode.VALIDATE_ERROR, validateMessage);
+        failResponse.setTraceId(trace.getTraceId());
+        return new ResponseEntity<>(failResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Response<Object>> handleNotReadException(HttpMessageNotReadableException notReadableException) {
+        final Trace trace = TraceIdUtils.getTrace();
+        logger.error("请求Id:{}, SpanId:{}, 错误码:{}, 错误信息:{}, 详细信息:{}", trace.getTraceId(), trace.getSpanId(),
+                ResponseCode.READ_JSON_ERROR.getCode(), ResponseCode.READ_JSON_ERROR.getMessage(), notReadableException.getMessage());
+        if (logger.isDebugEnabled()) {
+            logger.error(notReadableException.getMessage(), notReadableException);
+        }
+        final Response<Object> failResponse = Response.fail(ResponseCode.READ_JSON_ERROR, ResponseCode.READ_JSON_ERROR.getMessage());
+        failResponse.setTraceId(trace.getTraceId());
+        return new ResponseEntity<>(failResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Response<Object>> handleParameterException(MissingServletRequestParameterException misException) {
+        final Trace trace = TraceIdUtils.getTrace();
+        String missParams = String.format("%s参数, 类型%s缺失", misException.getParameterName(),misException.getParameterType());
+        logger.error("请求Id:{} ,SpanId:{} ,详细信息:{}",trace.getTraceId(), trace.getSpanId(), missParams);
+        if (logger.isDebugEnabled()) {
+            logger.error(misException.getMessage(), misException);
+        }
+        final Response<Object> failResponse = Response.fail(ResponseCode.INVALID_PARAMETER, missParams);
         failResponse.setTraceId(trace.getTraceId());
         return new ResponseEntity<>(failResponse, HttpStatus.BAD_REQUEST);
     }
@@ -57,7 +83,7 @@ public class ExceptionErrorHandler {
     }
 
     @ExceptionHandler(SysException.class)
-    public ResponseEntity<Response<Object>> handleBizException(SysException sysException) {
+    public ResponseEntity<Response<Object>> handleSysException(SysException sysException) {
         final Trace trace = TraceIdUtils.getTrace();
         int errCode = sysException.getErrCode();
         String message = sysException.getMessage();
@@ -72,7 +98,7 @@ public class ExceptionErrorHandler {
     }
 
     @ExceptionHandler(RpasException.class)
-    public ResponseEntity<Response<Object>> handleBizException(RpasException rpasException) {
+    public ResponseEntity<Response<Object>> handleRpasException(RpasException rpasException) {
         final Trace trace = TraceIdUtils.getTrace();
         int errCode = rpasException.getErrCode();
         String message = rpasException.getMessage();
@@ -87,7 +113,7 @@ public class ExceptionErrorHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Response<Object>> handleBizException(Exception exception) {
+    public ResponseEntity<Response<Object>> handleException(Exception exception) {
         final Trace trace = TraceIdUtils.getTrace();
         logger.error("请求ID:{}, SpanId:{}, 未知异常:{}, 详细信息:{}", trace.getTraceId(), trace.getSpanId(), exception.getMessage(), exception);
         if (logger.isDebugEnabled()) {
@@ -97,6 +123,4 @@ public class ExceptionErrorHandler {
         failResponse.setTraceId(trace.getTraceId());
         return new ResponseEntity<>(failResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-
 }
