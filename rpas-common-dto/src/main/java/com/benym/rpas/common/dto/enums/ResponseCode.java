c6d4804b747eb34eb05f97ffb02b54c900110138
benym <38175888+benym@users.noreply.github.com>
2022-07-11 17:26:48 +0800

    [feature]同步异常code类型更新

--- rpas-common-dto/src/main/java/com/benym/rpas/common/dto/enums/ResponseCode.java
+++ rpas-common-dto/src/main/java/com/benym/rpas/common/dto/enums/ResponseCode.java
@@ -3,29 +3,28 @@ package com.benym.rpas.common.dto.enums;
 /**
  * @Time : 2022/7/6 21:18
  */
-
 public enum ResponseCode implements StatusCode {
 
-    SUCCESS(0, "请求成功"),
-    FAILED(500, "请求失败"),
-    BIZ_EXCEPTION_CODE(20000,"业务异常"),
-    SYS_EXCEPTION_CODE(20001,"系统异常"),
-    RPAS_EXCEPTION_CODE(20002,"系统内部异常"),
-    UNKNOWN_EXCEPTION_CODE(20003,"未知异常"),
-    VALIDATE_ERROR(20003, "参数校验失败"),
-    READ_JSON_ERROR(20004,"Json格式不正确"),
-    INVALID_PARAMETER(20005,"参数错误");
+    SUCCESS("Success", "请求成功"),
+    FAILED("Failed", "请求失败"),
+    BIZ_EXCEPTION_CODE("BizException","业务异常"),
+    SYS_EXCEPTION_CODE("SysException","系统异常"),
+    RPAS_EXCEPTION_CODE("InternalException","系统内部异常"),
+    UNKNOWN_EXCEPTION_CODE("UnknownException","未知异常"),
+    VALIDATE_ERROR("ValidateError", "参数校验失败"),
+    READ_JSON_ERROR("ReadJsonError","Json格式不正确"),
+    INVALID_PARAMETER("InvalidParameter","参数错误");
 
-    private final int code;
+    private final String code;
     private final String message;
 
-    ResponseCode(int code, String message) {
+    ResponseCode(String code, String message) {
         this.code = code;
         this.message = message;
     }
 
     @Override
-    public int getCode() {
+    public String getCode() {
         return this.code;
     }
 
