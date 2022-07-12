5632ae0f35892bc647d5d874ad02e0eb225b4d37
benym <38175888+benym@users.noreply.github.com>
2022-07-12 19:29:10 +0800

    Update TraceFilter.java

--- rpas-common-core/src/main/java/com/benym/rpas/common/core/trace/TraceFilter.java
+++ rpas-common-core/src/main/java/com/benym/rpas/common/core/trace/TraceFilter.java
@@ -1,5 +1,8 @@
 package com.benym.rpas.common.core.trace;
 
+import com.benym.rpas.common.dto.enums.Trace;
+import com.benym.rpas.common.utils.SnowflakeUtils;
+import com.benym.rpas.common.utils.TraceIdUtils;
 import java.io.IOException;
 import java.util.Objects;
 import javax.servlet.Filter;
@@ -9,9 +12,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import com.benym.rpas.common.dto.enums.Trace;
-import com.benym.rpas.common.utils.SnowflakeUtils;
-import com.benym.rpas.common.utils.TraceIdUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -35,17 +35,17 @@ public class TraceFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         Trace trace = TraceIdUtils.getTrace();
-        TraceWrapper traceWrapper = null;
+        TraceRequestWrapper traceRequestWrapper = null;
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpServletRequest = (HttpServletRequest) request;
             if (httpServletRequest.getHeader(Trace.TRACE_ID) == null) {
-                traceWrapper = new TraceWrapper(httpServletRequest);
-                traceWrapper.putHeader(Trace.TRACE_ID, trace.getTraceId());
-                traceWrapper.putHeader(Trace.SPAN_ID, trace.getSpanId());
+                traceRequestWrapper = new TraceRequestWrapper(httpServletRequest);
+                traceRequestWrapper.putHeader(Trace.TRACE_ID, trace.getTraceId());
+                traceRequestWrapper.putHeader(Trace.SPAN_ID, trace.getSpanId());
             }
         }
         try {
-            chain.doFilter(Objects.nonNull(traceWrapper) ? traceWrapper : request, response);
+            chain.doFilter(Objects.nonNull(traceRequestWrapper) ? traceRequestWrapper : request, response);
             // doFilter之后表示当前请求结束，下一次属于另外一个Span
             MDC.put(Trace.SPAN_ID, String.valueOf(SnowflakeUtils.get().next()));
             // 请求完成之后同步清理traceId
