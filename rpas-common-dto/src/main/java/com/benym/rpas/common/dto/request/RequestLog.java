cc10d12b4d2e135a6833ab6b68344b22661bd250
benym <38175888+benym@users.noreply.github.com>
2022-07-12 19:03:17 +0800

    Update RequestLog.java

--- rpas-common-dto/src/main/java/com/benym/rpas/common/dto/request/RequestLog.java
+++ rpas-common-dto/src/main/java/com/benym/rpas/common/dto/request/RequestLog.java
@@ -5,4 +5,137 @@ package com.benym.rpas.common.dto.request;
  */
 public class RequestLog {
 
+    /**
+     * 请求Url
+     */
+    private String requestUrl;
+
+    /**
+     * 调用者地址
+     */
+    private String remoteAddr;
+
+    /**
+     * 请求头
+     */
+    private Object requestHeaders;
+
+    /**
+     * 被调用方法
+     */
+    private String method;
+
+    /**
+     * 请求参数
+     */
+    private Object requestParams;
+
+    /**
+     * 响应状态
+     */
+    private Integer status;
+
+    /**
+     * 响应头
+     */
+    private Object responseHeaders;
+
+    /**
+     * 响应数据
+     */
+    private Object response;
+
+    /**
+     * 接口耗时
+     */
+    private Long totalTime;
+
+    public RequestLog() {
+    }
+
+    public RequestLog(String requestUrl, String remoteAddr, Object requestHeaders,
+            String method, Object requestParams, Integer status, Object responseHeaders,
+            Object response, Long totalTime) {
+        this.requestUrl = requestUrl;
+        this.remoteAddr = remoteAddr;
+        this.requestHeaders = requestHeaders;
+        this.method = method;
+        this.requestParams = requestParams;
+        this.status = status;
+        this.responseHeaders = responseHeaders;
+        this.response = response;
+        this.totalTime = totalTime;
+    }
+
+    public String getRequestUrl() {
+        return requestUrl;
+    }
+
+    public void setRequestUrl(String requestUrl) {
+        this.requestUrl = requestUrl;
+    }
+
+    public String getRemoteAddr() {
+        return remoteAddr;
+    }
+
+    public void setRemoteAddr(String remoteAddr) {
+        this.remoteAddr = remoteAddr;
+    }
+
+    public Object getRequestHeaders() {
+        return requestHeaders;
+    }
+
+    public void setRequestHeaders(Object requestHeaders) {
+        this.requestHeaders = requestHeaders;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public Object getRequestParams() {
+        return requestParams;
+    }
+
+    public void setRequestParams(Object requestParams) {
+        this.requestParams = requestParams;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Object getResponseHeaders() {
+        return responseHeaders;
+    }
+
+    public void setResponseHeaders(Object responseHeaders) {
+        this.responseHeaders = responseHeaders;
+    }
+
+    public Object getResponse() {
+        return response;
+    }
+
+    public void setResponse(Object response) {
+        this.response = response;
+    }
+
+    public Long getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(Long totalTime) {
+        this.totalTime = totalTime;
+    }
 }
