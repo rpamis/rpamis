ce6c30ea4b477ef37a54487b0991972a6718a74c
benym <38175888+benym@users.noreply.github.com>
2022-07-12 19:28:41 +0800

    Update TraceResponseWrapper.java

--- rpas-common-core/src/main/java/com/benym/rpas/common/core/trace/TraceResponseWrapper.java
+++ rpas-common-core/src/main/java/com/benym/rpas/common/core/trace/TraceResponseWrapper.java
@@ -1,1 +1,20 @@
-1
+package com.benym.rpas.common.core.trace;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+/**
+ * @date 2022/7/12 7:28 下午
+ */
+public class TraceResponseWrapper extends HttpServletResponseWrapper {
+
+    /**
+     * Constructs a response adaptor wrapping the given response.
+     *
+     * @param response The response to be wrapped
+     * @throws IllegalArgumentException if the response is null
+     */
+    public TraceResponseWrapper(HttpServletResponse response) {
+        super(response);
+    }
+}
