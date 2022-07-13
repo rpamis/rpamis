4ad96b80079df58d4566e22525e842c39df78375
benym <38175888+benym@users.noreply.github.com>
2022-07-13 11:18:22 +0800

    [feature]增加远程调用日志打印

--- rpas-common-core/src/main/java/com/benym/rpas/common/core/trace/FeignRequestInterceptor.java
+++ rpas-common-core/src/main/java/com/benym/rpas/common/core/trace/FeignRequestInterceptor.java
@@ -4,6 +4,8 @@ import com.benym.rpas.common.dto.enums.Trace;
 import com.benym.rpas.common.utils.TraceIdUtils;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +15,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignRequestInterceptor implements RequestInterceptor {
 
+    private static final Logger logger = LoggerFactory.getLogger(FeignRequestInterceptor.class);
+
     /**
      * 为远程调用增加traceId
      */
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        logger.info("开始远程调用{}", requestTemplate);
         String traceId = MDC.get(Trace.TRACE_ID);
         if (traceId != null) {
             requestTemplate.header(Trace.TRACE_ID, traceId);
