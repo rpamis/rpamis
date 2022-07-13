e544ab842a90ba2b4ff7d6d7ad33285ebc6777aa
benym <benyuanming@gmail.com>
2022-07-13 22:23:36 +0800

    [init]初始化模块

---
+++ rpas-architecture-build/src/main/java/com/benym/rpas/architecture/RpasArchitectureBuildApplication.java
@@ -0,0 +1,13 @@
+package com.benym.rpas.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpasArchitectureBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpasArchitectureBuildApplication.class, args);
+    }
+
+}
---
+++ rpas-architecture-build/src/test/java/com/benym/rpas/architecture/RpasArchitectureBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benym.rpas.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpasArchitectureBuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
