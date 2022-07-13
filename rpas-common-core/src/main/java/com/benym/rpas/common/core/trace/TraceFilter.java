8fe2472290be16f4bdbf5857264b5dcccf0f0501
benym <38175888+benym@users.noreply.github.com>
2022-07-13 10:24:37 +0800

    [feature]增加log开关

--- rpas-common-core/src/main/java/com/benym/rpas/common/core/trace/TraceFilter.java
+++ rpas-common-core/src/main/java/com/benym/rpas/common/core/trace/TraceFilter.java
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -31,6 +32,10 @@ public class TraceFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(TraceFilter.class);
 
+
+    @Value("${trace.log.activate:true}")
+    private static boolean traceLogActivate;
+
     public TraceFilter() {
     }
 
@@ -39,23 +44,27 @@ public class TraceFilter implements Filter {
             throws IOException, ServletException {
         Trace trace = TraceIdUtils.getTrace();
         TraceRequestWrapper traceRequestWrapper;
+        RequestLog requestLog = null;
         TraceResponseWrapper traceResponseWrapper = new TraceResponseWrapper((HttpServletResponse) response);
-        RequestLog requestLog;
         traceRequestWrapper = new TraceRequestWrapper((HttpServletRequest) request);
+        if (traceLogActivate) {
+            requestLog = LoggerHttp.init(traceRequestWrapper);
+        }
         // 初始化入参信息
-        requestLog = LoggerHttp.init(traceRequestWrapper);
         traceRequestWrapper.putHeader(Trace.TRACE_ID, trace.getTraceId());
         traceRequestWrapper.putHeader(Trace.SPAN_ID, trace.getSpanId());
         try {
             chain.doFilter(traceRequestWrapper, response);
-            // 更新出参信息
-            LoggerHttp.update(requestLog, traceResponseWrapper);
+            if (traceLogActivate) {
+                // 更新出参信息
+                LoggerHttp.update(requestLog, traceResponseWrapper);
+                // 打印请求日志，包括入参、出参
+                logger.info(JSONUtil.toJsonStr(requestLog));
+            }
             // doFilter之后表示当前请求结束，下一次属于另外一个Span
             MDC.put(Trace.SPAN_ID, String.valueOf(SnowflakeUtils.get().next()));
             // 请求完成之后同步清理traceId
             TraceIdUtils.clearTrace();
-            // 打印请求日志，包括入参、出参
-            logger.info(JSONUtil.toJsonStr(requestLog));
         } catch (Exception e) {
             logger.warn("traceId Filter error{}", e.getMessage());
         }
