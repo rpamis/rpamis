c2defe2ec7a179da2f001e1f4360a77fab222a79
benym <38175888+benym@users.noreply.github.com>
2022-07-20 17:27:53 +0800

    Create BuildServiceImpl.java

---
+++ rpas-architecture-build/src/main/java/com/benym/rpas/architecture/service/impl/BuildServiceImpl.java
@@ -0,0 +1,18 @@
+package com.benym.rpas.architecture.service.impl;
+
+import com.benym.rpas.architecture.config.BaseProjectConfig;
+import com.benym.rpas.architecture.pojo.FileVO;
+import com.benym.rpas.architecture.service.BuildService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @date 2022/7/20 4:48 下午
+ */
+@Service
+public class BuildServiceImpl implements BuildService {
+
+    @Override
+    public FileVO architectureBuild(BaseProjectConfig baseProjectConfig) {
+        return null;
+    }
+}
