9789dce7969bd6159b4d96efaabbedaf8410fb04
benym <38175888+benym@users.noreply.github.com>
2022-07-25 18:49:55 +0800

    Update StarterTemplate.java

--- rpas-architecture-build/src/main/java/com/benym/rpas/architecture/template/StarterTemplate.java
+++ rpas-architecture-build/src/main/java/com/benym/rpas/architecture/template/StarterTemplate.java
@@ -21,17 +21,17 @@ public class StarterTemplate extends BuildAbstractTemplate {
     }
 
     @Override
-    protected void initPath(BaseProjectConfig baseProjectConfig) {
+    protected void initPath() {
 
     }
 
     @Override
-    protected void resolve(BaseProjectConfig baseProjectConfig) {
+    protected void resolve() {
 
     }
 
     @Override
-    protected FileVO create(BaseProjectConfig baseProjectConfig) {
+    protected FileVO create() {
         return null;
     }
 }
