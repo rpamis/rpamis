652c0d6cdc2dfe83ce54461294f2c07e54effb00
benym <38175888+benym@users.noreply.github.com>
2022-07-26 21:22:56 +0800

    Create ProjectTemplate.java

---
+++ rpas-architecture-build/src/main/java/com/benym/rpas/architecture/consts/ProjectTemplate.java
@@ -0,0 +1,11 @@
+package com.benym.rpas.architecture.consts;
+
+/**
+ * @date 2022/7/26 4:41 下午
+ */
+public interface ProjectTemplate {
+
+    String APPLICATION_YAML_NAME = "{yamlResourcePath}bootstrap.yaml.ftl";
+
+    String BOOTSTRAP_YAML_NAME = "{yamlResourcePath}bootstrap.yaml.ftl";
+}
