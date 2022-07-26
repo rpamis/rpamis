a8db49665d615a017970ee8fbf1591528b0e8808
benym <38175888+benym@users.noreply.github.com>
2022-07-26 21:22:23 +0800

    Update ProjectKey.java

--- rpas-architecture-build/src/main/java/com/benym/rpas/architecture/consts/ProjectKey.java
+++ rpas-architecture-build/src/main/java/com/benym/rpas/architecture/consts/ProjectKey.java
@@ -5,6 +5,33 @@ package com.benym.rpas.architecture.consts;
  */
 public interface ProjectKey {
 
-    String YAML_RESOURCE_PATH = "YAML_RESROUCE_PATH";
+    String YAML_RESOURCE_PATH = "yamlResourcePath";
+
+    String API_PACKAGE_PATH = "apiPackagePath";
+    String DAO_RESOURCE_PATH = "daoResourcePath";
+    String DAO_PACKAGE_PATH = "daoPackagePath";
+    String SERVICE_PACKAGE_PATH = "servicePackagePath";
+    String WEB_APPLICATION_PATH = "webApplicationPath";
+    String WEB_RESOURCE_PATH = "webResourcePath";
+    String WEB_PACKAGE_PATH = "webPackagePath";
+    String API_MOUDULE_API_DIR = "apiMouduleApiDir";
+    String API_MOUDULE_POJO_DIR = "apiMoudulePojoDir";
+    String API_MOUDULE_DTO_DIR = "apiMouduleDtoDir";
+    String API_MOUDULE_VO_DIR = "apiMouduleVoDir";
+    String API_MOUDULE_QUERY_DIR = "apiMouduleQueryDir";
+    String DAO_MOUDULE_DAO_DIR = "daoMouduleDaoDir";
+    String DAO_MOUDULE_DAO_IMPL_DIR = "daoMouduleDaoImplDir";
+    String DAO_MOUDULE_MAPPER_DIR = "daoMouduleMapperDir";
+    String DAO_MOUDULE_DO_DIR = "daoMouduleDoDir";
+    String DAO_MOUDULE_MAPPER_XML_DIR = "daoMouduleMapperXmlDir";
+    String SERVICE_MOUDULE_SERVICE_DIR = "serviceMouduleServiceDir";
+    String SERVICE_MOUDULE_SERVICE_IMPL_DIR = "serviceMouduleServiceImplDir";
+    String SERVICE_MOUDULE_POJO_DIR = "serviceMoudulePojoDir";
+    String SERVICE_MOUDULE_DTO_DIR = "serviceMouduleDtoDir";
+    String SERVICE_MOUDULE_VO_DIR = "serviceMouduleVoDir";
+    String SERVICE_MOUDULE_QUERY_DIR = "serviceMouduleQueryDir";
+    String WEB_MOUDULE_CONTROLLER_DIR = "webMouduleControllerDir";
+    String WEB_MOUDULE_APPLICAITION_DIR = "webMouduleApplicationDir";
+    String WEB_MOUDULE_RESOURCE_DIR = "webMouduleResourceDir";
 
 }
