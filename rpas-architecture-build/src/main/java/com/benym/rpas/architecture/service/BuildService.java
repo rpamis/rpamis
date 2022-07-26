1ab6be6ae1ffe096429fae435797346db078257e
benym <38175888+benym@users.noreply.github.com>
2022-07-26 21:24:08 +0800

    Update BuildService.java

--- rpas-architecture-build/src/main/java/com/benym/rpas/architecture/service/BuildService.java
+++ rpas-architecture-build/src/main/java/com/benym/rpas/architecture/service/BuildService.java
@@ -2,6 +2,7 @@ package com.benym.rpas.architecture.service;
 
 import com.benym.rpas.architecture.config.BaseProjectConfig;
 import com.benym.rpas.architecture.pojo.FileVO;
+import java.io.File;
 
 /**
  * @date 2022/7/20 4:36 下午
@@ -11,5 +12,7 @@ public interface BuildService {
     FileVO architectureBuild(BaseProjectConfig baseProjectConfig);
 
     void download(String id);
+
+    void generate(File file, String templatesFtl, BaseProjectConfig baseProjectConfig);
 
 }
