fd93ed28d15ec0de334be38bf587db58dd456153
benym <38175888+benym@users.noreply.github.com>
2022-07-27 18:00:50 +0800

    Update ArchitectureBuildController.java

--- rpas-architecture-build/src/main/java/com/benym/rpas/architecture/controller/ArchitectureBuildController.java
+++ rpas-architecture-build/src/main/java/com/benym/rpas/architecture/controller/ArchitectureBuildController.java
@@ -6,16 +6,19 @@ import com.benym.rpas.architecture.service.BuildService;
 import com.benym.rpas.common.dto.response.Response;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @date 2022/7/20 4:47 下午
  */
 @RestController
+@RequestMapping("/rpas")
 public class ArchitectureBuildController {
 
     @Autowired
@@ -23,7 +26,7 @@ public class ArchitectureBuildController {
 
     @ApiOperation("项目生成")
     @PostMapping("/build")
-    public Response<String> build(@RequestBody BaseProjectConfig baseProjectConfig) {
+    public Response<String> build(@RequestBody @Validated BaseProjectConfig baseProjectConfig) {
         FileVO fileVO = buildService.architectureBuild(baseProjectConfig);
         return Response.success(fileVO.getFilePath());
     }
