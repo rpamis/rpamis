ba7f3c1a5ccf898d97e8a0c1c3cd9d4bb3ecb0af
benym <benyuanming@gmail.com>
2022-11-23 22:13:58 +0800

    feature:增加usage模块，utils示例代码

---
+++ rpas-usage-test/src/main/java/com/benym/rpas/usage/test/utils/entity/EntityTarget.java
@@ -0,0 +1,51 @@
+package com.benym.rpas.usage.test.utils.entity;
+
+import java.io.Serializable;
+
+/**
+ * @date 2022/11/23 20:54
+ */
+public class EntityTarget implements Serializable {
+
+    private static final long serialVersionUID = 8449069812090444133L;
+
+    private String id;
+
+    private Integer age;
+
+    private Long height;
+
+    private Byte Status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getHeight() {
+        return height;
+    }
+
+    public void setHeight(Long height) {
+        this.height = height;
+    }
+
+    public Byte getStatus() {
+        return Status;
+    }
+
+    public void setStatus(Byte status) {
+        Status = status;
+    }
+}
