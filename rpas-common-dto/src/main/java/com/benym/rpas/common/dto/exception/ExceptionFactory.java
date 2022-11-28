780048f7157c9307ae40b6ef280ae5c102353f6d
benym <benyuanming@gmail.com>
2022-11-28 21:18:31 +0800

    feature:异常工厂支持MethodHandles

--- rpas-common-dto/src/main/java/com/benym/rpas/common/dto/exception/ExceptionFactory.java
+++ rpas-common-dto/src/main/java/com/benym/rpas/common/dto/exception/ExceptionFactory.java
@@ -2,7 +2,11 @@ package com.benym.rpas.common.dto.exception;
 
 import com.benym.rpas.common.dto.enums.StatusCode;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 异常工厂
@@ -11,25 +15,44 @@ import java.lang.reflect.Constructor;
  */
 public class ExceptionFactory {
 
+    private static final MethodHandles.Lookup publicLookup = MethodHandles.publicLookup();
+
+    private static final MethodType methodType = MethodType.methodType(void.class, String.class);
+
+    private static final ConcurrentHashMap<String, MethodHandle> cacheException = new ConcurrentHashMap<>();
+
+    /**
+     * 方法句柄是一个有类型的，可以直接执行的指向底层方法、构造器、field等的引用
+     * 可以简单理解为函数指针，它是一种更加底层的查找、调整和调用方法的机制
+     *
+     * 提供比反射更高效的映射方法
+     * 效率上与原生调用仅有纳秒级差距
+     *
+     * @param cls 动态推断的class
+     * @param message 需要抛出给前端的信息
+     * @param <T> class类型
+     * @return AbstractException或其子类
+     */
     public static <T extends AbstractException> AbstractException getException(Class<T> cls, String message) {
-        Class<?>[] parameterTypes = {String.class};
-        Constructor<T> constructor;
-        T t;
         try {
-            constructor = cls.getConstructor(parameterTypes);
-            Object[] parameters = new Object[]{message};
-            t = constructor.newInstance(parameters);
-        } catch (Exception e) {
-            throw sysException("获取cache exception异常", e);
+            MethodHandle methodHandle = cacheException.get(cls.toString());
+            if (methodHandle != null) {
+                return (AbstractException) methodHandle.invoke(message);
+            }
+            methodHandle = publicLookup.findConstructor(cls, methodType);
+            cacheException.putIfAbsent(cls.toString(), methodHandle);
+            Object invoke = methodHandle.invoke(message);
+            return (AbstractException) invoke;
+        } catch (Throwable throwable) {
+            throw sysException("获取cache exception异常", throwable);
         }
-        return t;
     }
 
     public static BizException bizException() {
         return new BizException();
     }
 
-    public static BizException bizException(String errMessage){
+    public static BizException bizException(String errMessage) {
         return new BizException(errMessage);
     }
 
@@ -78,11 +101,11 @@ public class ExceptionFactory {
     }
 
     public static RpasException rpasException(String errCode, String errMessage, String detailMessage) {
-        return new RpasException(errCode,errMessage,detailMessage);
+        return new RpasException(errCode, errMessage, detailMessage);
     }
 
     public static RpasException rpasException(String errCode, String errMessage) {
-        return new RpasException(errCode,errMessage);
+        return new RpasException(errCode, errMessage);
     }
 
     public static RpasException rpasException(StatusCode statusCode) {
