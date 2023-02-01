17f1fd07be4153ba860958c12ecc7c519123b664
benym <benyuanming@gmail.com>
2023-02-01 18:04:05 +0800

    fix(exception): 清除坏味道

--- rpamis-common-dto/src/main/java/com/benym/rpamis/common/dto/validation/ValidatedAction.java
+++ rpamis-common-dto/src/main/java/com/benym/rpamis/common/dto/validation/ValidatedAction.java
@@ -1,6 +1,8 @@
 package com.benym.rpamis.common.dto.validation;
 
 /**
+ * 分组校验interface基类
+ *
  * @author benym
  * @date 2023/1/31 17:45
  */
--- rpamis-common-exception/src/main/java/com/benym/rpamis/common/exception/annotation/SpecifiesValueValidatorImpl.java
+++ rpamis-common-exception/src/main/java/com/benym/rpamis/common/exception/annotation/SpecifiesValueValidatorImpl.java
@@ -4,6 +4,7 @@ import com.benym.rpamis.common.dto.exception.ExceptionFactory;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -48,37 +49,45 @@ public class SpecifiesValueValidatorImpl implements ConstraintValidator<Specifie
                 return true;
             }
             if (enumClass.isEnum()) {
-                // 获取传入的枚举class的所有定义的枚举，反射获取code判断是否和入参相同
-                Object[] enumConstants = enumClass.getEnumConstants();
-                for (Object enumConstant : enumConstants) {
-                    Method method = enumClass.getDeclaredMethod("getCode");
-                    Object invokeResult = method.invoke(enumConstant);
-                    if (invokeResult.equals(value)) {
-                        return true;
-                    }
-                }
-            } else if (value instanceof String) {
-                if (strSet.contains(value)) {
-                    return true;
-                }
-            } else if (value instanceof Integer) {
-                if (intSet.contains(value)) {
-                    return true;
-                }
-            } else if (value instanceof List) {
-                for (Object v : (List<?>) value) {
-                    String cast = (String) v;
-                    if (!strSet.contains(cast)) {
-                        return false;
-                    }
-                }
+                return validEnum(value, enumClass);
+            }
+            if (value instanceof String && strSet.contains(value)) {
                 return true;
             }
+            if (value instanceof Integer && intSet.contains(value)) {
+                return true;
+            }
+            if (value instanceof List) {
+                return validList(value, strSet);
+            }
         } catch (NoSuchMethodException e) {
-            throw ExceptionFactory.sysException("该枚举类没有getCode方法", e);
+            throw ExceptionFactory.sysException(enumClass + "枚举类没有getCode方法", e);
         } catch (Exception e) {
             throw ExceptionFactory.sysException("特定值校验器异常", e);
         }
         return false;
+    }
+
+    public static boolean validEnum(Object value, Class<?> enumClass) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        // 获取传入的枚举class的所有定义的枚举，反射获取code判断是否和入参相同
+        Object[] enumConstants = enumClass.getEnumConstants();
+        for (Object enumConstant : enumConstants) {
+            Method method = enumClass.getDeclaredMethod("getCode");
+            Object invokeResult = method.invoke(enumConstant);
+            if (invokeResult.equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean validList(Object value, Set<String> strSet) {
+        for (Object v : (List<?>) value) {
+            String cast = (String) v;
+            if (!strSet.contains(cast)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
