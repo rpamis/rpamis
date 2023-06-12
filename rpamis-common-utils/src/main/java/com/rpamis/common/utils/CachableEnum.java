1a1cb199251aaabaddcd5b8e573a288ee45ecf67
benym <benyuanming@gmail.com>
2023-06-12 17:27:40 +0800

    feature: 增加通用枚举缓存工具类，增加ClassScanner工具类

---
+++ rpamis-common-utils/src/main/java/com/rpamis/common/utils/CachableEnum.java
@@ -0,0 +1,24 @@
+package com.rpamis.common.utils;
+
+/**
+ * 通用缓存枚举接口
+ *
+ * @author benym
+ * @date 2023/6/12 14:01
+ */
+public interface CachableEnum<C, D> {
+
+    /**
+     * 获取Code
+     *
+     * @return {@link Object}
+     */
+    C getCode();
+
+    /**
+     * 获取Desc
+     *
+     * @return {@link Object}
+     */
+    D getDesc();
+}
