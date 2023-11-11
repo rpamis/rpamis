7aa42557723fcc8cc4c0b7b0d47983e54321a5fa
benym <benyuanming@gmail.com>
2023-11-11 23:17:40 +0800

    feat: 完善spi依赖注入逻辑

--- rpamis-extension-spi/src/main/java/com/rpamis/extension/spi/SpiLoader.java
+++ rpamis-extension-spi/src/main/java/com/rpamis/extension/spi/SpiLoader.java
@@ -1,8 +1,11 @@
 package com.rpamis.extension.spi;
 
 import com.rpamis.extension.spi.loading.SpiLoadingStrategy;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +13,8 @@ import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.StreamSupport;
 
 /**
@@ -19,6 +24,8 @@ import java.util.stream.StreamSupport;
  * @date 2023/10/31 21:52
  */
 public class SpiLoader<T> {
+
+  private static final Logger LOGGER = Logger.getLogger(SpiLoader.class.getName());
 
   private final Class<?> type;
 
@@ -112,7 +119,7 @@ public class SpiLoader<T> {
     }
     Object instance = container.getValue();
     if (instance == null) {
-      synchronized (container) {
+      synchronized (container.getValue()) {
         instance = container.getValue();
         if (instance == null) {
           instance = this.createSpiImpl(name);
@@ -155,7 +162,99 @@ public class SpiLoader<T> {
    * @param spiImplInstance spiImplInstance
    */
   private void injectSpiImpl(T spiImplInstance) {
+    if (pluginInjector == null) {
+      return;
+    }
+    try {
+      // 获取spi实现类的所有method，找到spi实现类中依赖的其他通用为spi的实例，并进行依赖注入
+      for (Method method : spiImplInstance.getClass().getMethods()) {
+        if (!isSetter(method)) {
+          continue;
+        }
+        Class<?> parameterType = method.getParameterTypes()[0];
+        if (isPrimitives(parameterType)) {
+          continue;
+        }
+        // 根据setter方法注入实体
+        inject(parameterType, method, spiImplInstance);
+      }
+    } catch (Exception e) {
+      String exceptionMessage = String.format("%s inject failed, because: %s", spiImplInstance, e);
+      LOGGER.log(Level.SEVERE, exceptionMessage);
+    }
+  }
 
+  /**
+   * 依赖注入，让Spi实现类中依赖的其他Spi实现类通过setter方法注入
+   *
+   * @param parameterType 参数类型Class
+   * @param method 对应方法
+   * @param spiImplInstance spi实现类实例
+   */
+  public void inject(Class<?> parameterType, Method method, T spiImplInstance) {
+    try {
+      String setterPropertyName = this.getSetterProperty(method);
+      // 根据参数类型如Protocol和属性名字如protocol获取应该注入的对象
+      Object object = pluginInjector.getSpiImpl(parameterType, setterPropertyName);
+      if (object != null) {
+        // 能够走到这里，说明method为set方法的method，因为如果不是需要注入的spi实现类对象，上述object为null
+        // 直接invoke，将object通过set赋值给需要依赖注入的spi实例
+        method.invoke(spiImplInstance, object);
+      }
+    } catch (Exception e) {
+      String exceptionMessage = String.format(
+          "Failed to inject via method %s  of interface %s, because: %s", method.getName(),
+          type.getName(), e);
+      LOGGER.log(Level.SEVERE, exceptionMessage);
+    }
+  }
+
+  /**
+   * method是否是public、名称是否为set开头、是否只有一个参数
+   *
+   * @param method method
+   * @return boolean
+   */
+  private boolean isSetter(Method method) {
+    return method.getName().startsWith("set")
+        && method.getParameterTypes().length == 1
+        && Modifier.isPublic(method.getModifiers());
+  }
+
+  /**
+   * 判断是否是包装类型 如果class是Array则获取数组的组件Class，比如String[] 获取的是String.class
+   *
+   * @param cls cls
+   * @return boolean
+   */
+  private static boolean isPrimitives(Class<?> cls) {
+    while (cls.isArray()) {
+      cls = cls.getComponentType();
+    }
+    return isPrimitive(cls);
+  }
+
+  /**
+   * 判断是否是包装类型
+   *
+   * @param cls cls
+   * @return boolean
+   */
+  private static boolean isPrimitive(Class<?> cls) {
+    return cls.isPrimitive() || cls == String.class || cls == Boolean.class
+        || cls == Character.class
+        || Number.class.isAssignableFrom(cls) || Date.class.isAssignableFrom(cls);
+  }
+
+  /**
+   * 获取method setter方法的属性名称，比如setVersion，返回version 如果method setter方法的名称小于3，则返回""
+   *
+   * @param method method
+   * @return String
+   */
+  private String getSetterProperty(Method method) {
+    return method.getName().length() > 3 ? method.getName().substring(3, 4).toLowerCase()
+        + method.getName().substring(4) : "";
   }
 
   /**
